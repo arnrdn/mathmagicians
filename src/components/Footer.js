1e79bfcd794f3d65ecf69dce72c94f9d623b106d
rdnrn <rodina.arina00@gmail.com>
2022-01-18 17:49:56 +0300

    Finish styling and fix linters
    Linters didn't like the result of a SASS compiling extension, so I have deleted them manually

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => (
   <footer>
-    <p className="author">Made by Nana</p>
+    <p className="fter author">
+      Made by
+      <Link to="https://github.com/rdnrn"> Nana</Link>
+    </p>
+    <p className="fter"><Link to="https://icons8.com/icon/DchOf6erOUjJ/calculator">Calculator icon by Icons8</Link></p>
   </footer>
 );
 
